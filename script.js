81fc3f54e848352640fa9ecdf51fb397f0a74571
Salvatore Amplo <salvatoreamplo@gmail.com>
2025-10-08 16:12:08 +0200

    w3d3

---
+++ script.js
@@ -0,0 +1,36 @@
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskInput = document.getElementById("taskInput");
+const taskList = document.getElementById("taskList");
+
+addTaskBtn.addEventListener("click", addTask);
+taskInput.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") addTask();
+});
+
+function addTask() {
+  const taskText = taskInput.value.trim();
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+
+  const span = document.createElement("span");
+  span.textContent = taskText;
+
+  span.addEventListener("click", () => {
+    li.classList.toggle("completed");
+  });
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Elimina";
+  deleteBtn.classList.add("delete-btn");
+
+  deleteBtn.addEventListener("click", () => {
+    taskList.removeChild(li);
+  });
+
+  li.appendChild(span);
+  li.appendChild(deleteBtn);
+  taskList.appendChild(li);
+
+  taskInput.value = "";
+}
